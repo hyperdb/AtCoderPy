2965b154ea8109544b78ca34c226968d1ea31b16
hyperdb <hyperdb@gmail.com>
2022-12-16 18:46:33 +0900

    ✨解答を追加 - ABC-049-A.py - ABC-049-B.py

---
+++ ABC/042-100/042-050/ABC-049-A.py
@@ -0,0 +1,17 @@
+# ABC-049 A - 居合を終え、青い絵を覆う  /
+# https://atcoder.jp/contests/abc049/tasks/abc049_a
+#
+def getString():
+    return input()
+
+
+def main():
+    c = getString()
+    if c in ['a', 'i', 'u', 'e', 'o']:
+        print('vowel')
+    else:
+        print('consonant')
+
+
+if __name__ == "__main__":
+    main()
