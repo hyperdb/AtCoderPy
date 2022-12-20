2e6de0f5255356f3ac8dfe0e02a7d832f752f4f8
hyperdb <hyperdb@gmail.com>
2022-12-20 16:07:38 +0900

    ✨解答を追加 - ABC-051-A.py - ABC-051-B.py

---
+++ ABC/042-100/051-060/ABC-051-A.py
@@ -0,0 +1,15 @@
+# ABC-051 A - Haiku
+# https://atcoder.jp/contests/abc051/tasks/abc051_a
+#
+def getString():
+    return input()
+
+
+def main():
+    s = getString()
+
+    print(s.replace(',', ' '))
+
+
+if __name__ == "__main__":
+    main()
