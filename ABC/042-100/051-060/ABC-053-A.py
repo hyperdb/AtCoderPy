19aad84562cb1f112cbb12ad27feb6242f5febf9
hyperdb <hyperdb@gmail.com>
2022-12-22 19:09:25 +0900

    ✨解答を追加 - ABC-053-A.py - ABC-053-B.py

---
+++ ABC/042-100/051-060/ABC-053-A.py
@@ -0,0 +1,14 @@
+# ABC-053 A - ABC/ARC
+# https://atcoder.jp/contests/abc053/tasks/abc053_a
+#
+def getInt():
+    return int(input())
+
+
+def main():
+    x = getInt()
+    print('ABC' if x < 1200 else 'ARC')
+
+
+if __name__ == "__main__":
+    main()
