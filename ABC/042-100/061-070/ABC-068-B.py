879035d7e7d850e031e6ea874fdb47c3338e7343
hyperdb <hyperdb@gmail.com>
2022-12-31 01:39:53 +0900

    ✨解答を追加 - ABC-068-A.py - ABC-068-B.py

---
+++ ABC/042-100/061-070/ABC-068-B.py
@@ -0,0 +1,20 @@
+# ABC-068 B - Break Number
+# https://atcoder.jp/contests/abc068/tasks/abc068_b
+#
+def getInt():
+    return int(input())
+
+
+def main():
+    n = getInt()
+
+    m = 0
+    for i in range(0, (n // 2) + 1):
+        if 2 ** i <= n:
+            m = 2 ** i
+
+    print(m)
+
+
+if __name__ == "__main__":
+    main()
