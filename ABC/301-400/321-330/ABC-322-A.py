690c8a493e2cd798069e66cc2ef89562e7c6e9cf
hyperdb <hyperdb@gmail.com>
2023-10-30 14:10:55 +0900

    ✨解答を追加 - ABC-322-A.py - ABC-322-B.py - インデックスページを更新

---
+++ ABC/301-400/321-330/ABC-322-A.py
@@ -0,0 +1,20 @@
+# ABC-322 A - First ABC 2
+# https://atcoder.jp/contests/abc322/tasks/abc322_a
+#
+def getInt():
+    return int(input())
+
+
+def getString():
+    return input()
+
+
+def main():
+    n = getInt()
+    s = ' ' + getString()
+
+    print(s.find('ABC'))
+
+
+if __name__ == "__main__":
+    main()
