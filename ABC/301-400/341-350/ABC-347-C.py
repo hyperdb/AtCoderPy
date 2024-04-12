f323b4d26e605c72e52a30d0f1a2fe847f79b4ea
hyperdb <hyperdb@gmail.com>
2024-04-12 19:16:34 +0900

    ✨解答を追加 - ABC-347-C.py - インデックスページを更新

---
+++ ABC/301-400/341-350/ABC-347-C.py
@@ -0,0 +1,35 @@
+#
+#
+#
+def getIntMap():
+    return map(int, input().split())
+
+
+def getIntList():
+    return list(map(int, input().split()))
+
+
+def main():
+    n, a, b = getIntMap()
+    d = getIntList()
+
+    wd = a + b
+    d = [x % wd for x in d]
+    d = list(set(d))
+    d.sort()
+
+    r = False
+    c = len(d)
+    for i in range(c):
+        d.append(d[i] + wd)
+        s = i
+        e = i + c - 1
+
+        if d[e] - d[s] < a:
+            r = True
+
+    print('Yes' if r else 'No')
+
+
+if __name__ == "__main__":
+    main()
