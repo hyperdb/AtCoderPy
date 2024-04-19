5005668c85124da16a5ec844bc346fb57c0cb926
hyperdb <hyperdb@gmail.com>
2024-04-19 13:57:14 +0900

    ✏️ソース内のコメントを追加

--- ABC/301-400/341-350/ABC-342-C.py
+++ ABC/301-400/341-350/ABC-342-C.py
@@ -1,4 +1,4 @@
-# ABC-342 C - Many Replacement 
+# ABC-342 C - Many Replacement
 # https://atcoder.jp/contests/abc342/tasks/abc342_c
 #
 def getInt():
@@ -19,16 +19,19 @@ def main():
     q = getInt()
     cd = getStringListRow(q)
 
+    # 辞書を作成
     dic = {}
     a = ord('a')
     for c in range(26):
         dic[chr(a + c)] = chr(a + c)
 
+    # 辞書を書き換え
     for c, d in cd:
         keys = [k for k, v in dic.items() if v == c]
         for k in keys:
             dic[k] = d
 
+    # 文字列を変換
     r = []
     for c in s:
         r.append(dic[c])
