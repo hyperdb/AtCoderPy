be0ece765b22937fa07fc9f89dddfb5bf155b82e
hyperdb <hyperdb@gmail.com>
2024-06-16 23:28:32 +0900

    ✏️コメントを追加 - ABC-038-C

--- ABC/001-100/031-040/ABC-038-C.py
+++ ABC/001-100/031-040/ABC-038-C.py
@@ -17,6 +17,7 @@ def main():
     a = getIntList()
 
     b = []
+    # 単純増加ではない箇所を探す
     for i in range(1, N):
         if a[i] <= a[i - 1]:
             b.append(i)
@@ -24,6 +25,7 @@ def main():
 
     b = [0] + b
     r = 0
+    # ぞれぞれの箇所で組合せ数を求める
     for i in range(1, len(b)):
         c = b[i] - b[i - 1]
         r += c
